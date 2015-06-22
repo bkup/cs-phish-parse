bb3bed8b1895caf4fce657d10c36f8d395a1cab1
bkup <brentakennedy@mac-frigatebird.ad.sei.cmu.edu.wv.cc.cmu.edu>
2015-06-22 16:06:14 -0400

    Added excludes for subjects and bug fixes

--- cs-phish-parse.py
+++ cs-phish-parse.py
@@ -3,7 +3,7 @@
 import csv
 import argparse
 
-##This script is used to parse TSV files exported from Cobalt Strike's phihsing module.  
+##This script is used to parse TSV files exported from Cobalt Strike's phishing module.  
 ##The spearfishes and weblog files are required.
 ##An optional exclude list can be used to remove certain email addresses from calculations
 
@@ -11,7 +11,8 @@ import argparse
 parser = argparse.ArgumentParser(description='This script will parse multiple Cobalt Strike Phishing campaigns. Inputs need to be TSV files exported by Cobalt Strike.')
 parser.add_argument('-w', '--weblog', help='Weblog TSV file',required=True)
 parser.add_argument('-s','--spearphishes', help='Spearphishes TSV file', required=True)
-parser.add_argument('-e','--exclude', help='Exclude address file; one per line', required=False)
+parser.add_argument('-e','--excludeaddress', help='Exclude address file; one per line', required=False)
+parser.add_argument('-x','--excludesubject', help='Exclude subject file; one per line', required=False)
 args = parser.parse_args()
 
 #Each line in the TSV gets made into a dictionary item and added to the overall list
@@ -39,18 +40,10 @@ with open(args.weblog,'r') as f:
         tmp=[host,created_at,method,uri,response,size,handler,user_agent,token_wl,primary]
         listWeblog.append(dict(zip(keysWeblog,tmp)))
 
-#read in exclude list if it exists
-if args.exclude:
-    with open(args.exclude,'r') as f:
-        excludeList = [word.strip() for word in f] #strip the new line /n 
-    #remove excludes
-    tmptok=[]
-    for dictx in listSpearfishes:
-        if dictx["_to"] in excludeList:
-            tmptok.append(dictx["_to"])
-            listSpearfishes.remove(dictx)
     
-
+####
+#Functions
+####
 def calcThings(subj):
     totTargets=0
     uniqClicks=0
@@ -65,34 +58,74 @@ def calcThings(subj):
 
     return totTargets,uniqClicks,totClicks
 
-def getUniqueList(listy,dictvalue):
-    tmpList=[]
-    for x in listy:
-        tmpList.append(x[dictvalue])
-    return filter(None,sorted(list(set(tmpList))))
-
+#gets a column in the TSV file
 def getList(listy,dictvalue):
     tmpList=[]
     for x in listy:
         tmpList.append(x[dictvalue])
     return filter(None,sorted(list(tmpList)))
 
-#only works for listSpearfishes
-def getUniqueListBySubj(listy,dictvalue,subj):
+#like get list but with a condition for the values
+def getUniqueListByKey(listy,key,dictvalue,keyvalue):
     tmpList=[]
     for x in listy:
-        if x["_subject"] == subj:
+        if x[key] == keyvalue:
             tmpList.append(x[dictvalue])
     return filter(None,sorted(list(set(tmpList))))
 
+#removes rows from TSV file
+def removeDictFromList(listy,key,value):
+    index=0
+    while index<len(listy):
+        dictx=listy[index]
+        if dictx[key] == value:
+            listy.remove(dictx)
+        else:
+            index+=1
+
+#######################################
+#Main
 #######################################
 
+#excluding things
+if args.excludeaddress:
+    with open(args.excludeaddress,'r') as f:
+        excludeList = [word.strip() for word in f] #strip the new line /n 
+    #remove excluded emails
+    #remove lines in listspearphises that contain the exlcude subject
+    for x in excludeList:
+        #first get tokens that will be exluded so they can be removed from listWeblog
+        excludetokens = getUniqueListByKey(listSpearfishes,"_to","_token",x)
+        #remove lines containing excluded subjects
+        removeDictFromList(listSpearfishes,"_to",x)
+        #remove the tokens from weblog associated with those emails 
+        for y in excludetokens:
+            removeDictFromList(listWeblog,"_token_wl",y)
+
+if args.excludesubject:
+    with open(args.excludesubject,'r') as f:
+        excludeList = [word.strip() for word in f] #strip the new line /n 
+    #remove lines in listspearphises that contain the exlcude subject
+    for x in excludeList:
+        #first get tokens that will be exluded so they can be removed from listWeblog
+        excludetokens = getUniqueListByKey(listSpearfishes,"_subject","_token",x)
+        #remove lines containing excluded subjects
+        removeDictFromList(listSpearfishes,"_subject",x)
+        #remove the tokens from weblog associated with those subjects
+        for y in excludetokens:
+            removeDictFromList(listWeblog,"_token_wl",y)
+
+    
+
 #derive needed values from lists
-subjectList = getUniqueList(listSpearfishes,"_subject")
-tokensUniq = getUniqueList(listWeblog,"_token_wl")
+#set gives unique list
+subjectList = set(getList(listSpearfishes,"_subject"))
+tokensUniq = set(getList(listWeblog,"_token_wl"))
 tokensTot = getList(listWeblog,"_token_wl")
-targetsTot = getUniqueList(listSpearfishes,"_to")
+targetsTot = set(getList(listSpearfishes,"_to"))
 #show the goods
+print len(tokensUniq)
+print len(targetsTot)
 print "-" * 50
 print "Number of campaigns: %i" % len(subjectList)
 print "Overall unique click rate: %5.2f%%" % (len(tokensUniq) / float(len(targetsTot))*100)
@@ -102,9 +135,9 @@ for subj in subjectList:
     print "Campaign '%s' " % subj
     print "-" * 50
     print "Total emails sent: %i" % totTargets
-    print "Total unique targets: %i" % len(getUniqueListBySubj(listSpearfishes,"_to",subj))
+    print "Total unique targets: %i" % len(getUniqueListByKey(listSpearfishes,"_subject","_to",subj))
     print "Unique targets who clicked: %i" % uniqClicks
-    print "Unique click rate: %5.2f%%" % ((uniqClicks / float(len(getUniqueListBySubj(listSpearfishes,"_to",subj))))*100) 
+    print "Unique click rate: %5.2f%%" % ((uniqClicks / float(len(getUniqueListByKey(listSpearfishes,"_subject","_to",subj))))*100) 
     print "Total clicks: %i" % totClicks
 
 
